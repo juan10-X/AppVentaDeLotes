0d2e7b730aef85951ec6e5b3a7df9fffb7544dfe
juan10-X <herreracarrascojuan9@gmail.com>
2026-05-01 17:23:30 -0500

    agregue archivo i18n

---
+++ APPLOTE/appLote/screen/i18n.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const i18n = () => {
+  return (
+    <View>
+      <Text>i18n</Text>
+    </View>
+  )
+}
+
+export default i18n
